e48a8f280695916f99af15172e6a34c02731c654
Zeyad-Ashraf-77 <zeyadaltantawy365@gmail.com>
2025-10-15 00:46:15 +0300

    firest commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   <h1 className='text-center text-5xl bg-red-500 ' > hello in My Saraha app Zeyad ❤️❤️❤️✌️ </h1>
+    </>
+  )
+}
+
+export default App
